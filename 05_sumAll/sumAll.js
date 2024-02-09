da54ae3cb1d27ea9d8b317b521c5656972a8711c
spigkaht <benj@betterrentals.com.au>
2024-02-09 23:00:49 +1100

    Completed 05 sumAll exercise from Odin JavaScript exercises

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,20 @@
-const sumAll = function() {
+const sumAll = function (num1, num2) {
+  let sum = 0;
 
+  if (!Number.isInteger(num1) || !Number.isInteger(num2)) return "ERROR";
+  if (num1 < 0 || num2 < 0) return "ERROR";
+
+  if (num1 > num2) {
+    let num3 = num1;
+    num1 = num2;
+    num2 = num3;
+  }
+
+  for (let i = num1; i <= num2; i++) {
+    sum = sum + i;
+  }
+
+  return sum;
 };
 
 // Do not edit below this line
