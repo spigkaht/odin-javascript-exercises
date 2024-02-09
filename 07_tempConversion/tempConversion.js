a5ee25c9e37598a727a0a67b91bb857eef563bc3
spigkaht <benj@betterrentals.com.au>
2024-02-09 23:36:28 +1100

    Completed 07 tempConversion from Odin JavaScript Exercises

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,9 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(far) {
+  return Math.round(((far - 32) * (5/9)) * 10) / 10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(cel) {
+  return Math.round((cel * 1.8 + 32) * 10) / 10;
 };
 
 // Do not edit below this line
