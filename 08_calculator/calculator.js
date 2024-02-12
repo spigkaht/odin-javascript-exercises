7c533d6898ddf05ad84b11055f4d8ef3842f99f0
spigkaht <benj@betterrentals.com.au>
2024-02-13 08:19:20 +1100

    Completed 08 calculator exercise from Odin JavaScript Exercises

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,41 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+  let sum = 0;
+  for (let i = 0; i < array.length; i++) {
+    sum += array[i];
+  }
+  return sum;
 };
 
-const multiply = function() {
-
+const multiply = function(...args) {
+  let total = args[0][0];
+	for (let i = 1; i < args[0].length; i++) {
+    total *= args[0][i];
+  }
+  return total;
 };
 
-const power = function() {
-	
+const power = function(num, power) {
+  let total = num;
+  for (let i = 0; i < power - 1; i++) {
+    total *= num;
+  }
+  return total;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	let total = 1;
+  for (let i = num; i > 0; i--) {
+    total = total * i;
+  }
+  return total;
 };
 
 // Do not edit below this line
