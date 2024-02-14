b545261aa08a41361b749be319832deb99c796d0
spigkaht <benj@betterrentals.com.au>
2024-02-14 22:57:01 +1100

    Completed 09 palindromes exercise from Odin JavaScript Exercises

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,33 +1,6 @@
 const palindromes = function (string) {
-  const array = [...string];
-  const arrayLength = [...string];
-  const newArray = [];
-  const punctuation = [
-    ".",
-    ",",
-    ";",
-    ":",
-    "!",
-    "?",
-    '"',
-    "'",
-    "(",
-    ")",
-    "[",
-    "]",
-    "{",
-    "}",
-    "-",
-    "_",
-  ];
-  for (let i = arrayLength.length - 1; i >= 0; i--) {
-    if (!punctuation.includes(array[i])) {
-      const letter = array.shift();
-      newArray.unshift(letter);
-    }
-  }
-  const finalArray = newArray.join("");
-  return finalArray === string;
+  const newString = string.toLowerCase().replace(/[^a-z0-9]/g, "");
+  return newString.split("").reverse().join("") === newString;
 };
 
 // Do not edit below this line
