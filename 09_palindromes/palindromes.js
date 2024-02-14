b72dfa0ed453f2d3ed1ef9d7769933e638964863
spigkaht <benj@betterrentals.com.au>
2024-02-14 16:27:32 +1100

    About to redo exercise.. committing to GitHub so I can save progress of failed attempt

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,33 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+  const array = [...string];
+  const arrayLength = [...string];
+  const newArray = [];
+  const punctuation = [
+    ".",
+    ",",
+    ";",
+    ":",
+    "!",
+    "?",
+    '"',
+    "'",
+    "(",
+    ")",
+    "[",
+    "]",
+    "{",
+    "}",
+    "-",
+    "_",
+  ];
+  for (let i = arrayLength.length - 1; i >= 0; i--) {
+    if (!punctuation.includes(array[i])) {
+      const letter = array.shift();
+      newArray.unshift(letter);
+    }
+  }
+  const finalArray = newArray.join("");
+  return finalArray === string;
 };
 
 // Do not edit below this line
