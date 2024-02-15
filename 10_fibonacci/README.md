431ce8956158e88f239306464d3e0e69f00ed652
spigkaht <benj@betterrentals.com.au>
2024-02-15 17:08:19 +1100

    Completed 10 fibonacci exercise for Odin JavaScript Exercises

--- 10_fibonacci/README.md
+++ 10_fibonacci/README.md
@@ -10,3 +10,5 @@ Create a function that returns a specific member of the Fibonacci sequence:
 fibonacci(4); // returns the 4th member of the series: 3  (1, 1, 2, 3)
 fibonacci(6); // returns 8
 ```
+
+1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144 ... 
--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,17 @@
-const fibonacci = function() {
-
+const fibonacci = function (num) {
+  const array = [1, 1, 2];
+  const int = parseInt(num);
+  if (int > 0) {
+    for (let i = 2; i <= int; i++) {
+      array[i] = array[i - 1] + array[i - 2];
+    }
+  } else if (int < 0) {
+    return "OOPS";
+  } else return 0;
+  return array[int - 1];
 };
+
+// 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144 ...
 
 // Do not edit below this line
 module.exports = fibonacci;
